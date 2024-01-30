44948e01710572e1a79fa5cf3d378346916eeef3
crowdbotics-dev <engineering@crowdbotics.com>
2024-01-30 15:17:16 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Newmo
+
+admin.site.register(Newmo)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Newmo(models.Model):
+    "Generated Model"
+    aaaa = models.BigIntegerField()
+    vxx = models.BigIntegerField()
